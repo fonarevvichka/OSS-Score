ca7247c5f6760d2b3d0a206c48de56a933f3eab4
Vichka Fonarev <fonarev.vichka@gmail.com>
2021-09-30 13:49:03 -0400

    basic server with routes stored in alternate file

--- run.go
+++ run.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	server "go_exploring/server"
+	"net/http"
+	routes "go_exploring/server"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.GET("./pong", server.Pong)
+	router.GET("./pong", routes.Pong)
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	
+	router.NoRoute(func(c *gin.Context) {
+              c.AbortWithStatus(http.StatusNotFound)
+       })
+	router.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
