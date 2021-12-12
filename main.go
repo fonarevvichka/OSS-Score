19dcaabdc008277ea355c869de266650f78f2d61
vichka fonarev <fonarev.vichka@gmail.com>
2021-12-12 00:22:07 -0500

    remove Pong route from main

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/Pong", server.Pong)
 	router.POST("/catalog/:catalog/owner/:owner/name/:name", server.QueryProject)
 	router.GET("/catalog/:catalog/owner/:owner/name/:name/scoreType/:scoreType", server.GetCachedScore)
 	router.Run()
