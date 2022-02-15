dce43edf8a82d2eec2be8110e1e0e7d9576a0959
vichka fonarev <fonarev.vichka@gmail.com>
2022-02-15 17:17:26 -0500

    add sleeps between update and config

--- api/queryScore/update.sh
+++ api/queryScore/update.sh
@@ -5,7 +5,7 @@ go build -o main
 cp ../../util/mongo_cert.pem ./mongo_cert.pem
 zip -X -r lambda.zip main mongo_cert.pem
 aws lambda update-function-code --function-name queryScoreHandler --zip-file fileb://lambda.zip --architectures "x86_64" > /dev/null
-
+sleep 3s
 aws lambda update-function-configuration --function-name queryScoreHandler --handler main \
                                 --timeout 300 --environment "Variables={MONGO_URI=$MONGO_URI, SHELF_LIFE=$SHELF_LIFE}" \
                                 --runtime go1.x > /dev/null
@@ -21,7 +21,7 @@ cp -r ../../util/scores ./scores
 
 zip -X -r lambda.zip main mongo_cert.pem queries scores
 aws lambda update-function-code --function-name queryScore --zip-file fileb://lambda.zip --architectures "x86_64" > /dev/null
-
+sleep 3s
 aws lambda update-function-configuration --function-name queryScore --handler main \
                                 --timeout 300 --environment "Variables={GIT_PAT_1=$GIT_PAT_1, GIT_PAT_2=$GIT_PAT_2, GIT_PAT_3=$GIT_PAT_3, GIT_PAT_4=$GIT_PAT_4, GIT_PAT_5=$GIT_PAT_5, MONGO_URI=$MONGO_URI, SHELF_LIFE=$SHELF_LIFE}" \
                                 --runtime go1.x > /dev/null
