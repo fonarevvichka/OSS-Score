85bb1beeecb27913924fb7bd836c2fb83371d1dc
Vichka Fonarev <fonarev.vichka@gmail.com>
2022-02-17 13:51:25 -0500

    exploring message recieve from queue

--- api/queryScore/processing/queryScore.go
+++ api/queryScore/processing/queryScore.go
@@ -5,12 +5,17 @@ import (
 	"fmt"
 
 	runtime "github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	// "github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
 
-func handler(ctx context.Context, messageOutput sqs.ReceiveMessageOutput) error {
+type message struct {
+	Records []types.Message
+}
+
+func handler(ctx context.Context, messageOutput types.ReceiveMessageOutput) error {
 	fmt.Println(messageOutput)
-	fmt.Println(messageOutput.Messages[0])
+	// fmt.Println(messageOutput.Messages[0])
 	// util.QueryProject(repo.Catalog, repo.Owner, repo.Name, repo.TimeFrame)
 	return nil
 }
