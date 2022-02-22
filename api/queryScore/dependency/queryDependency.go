0dd44731e5c319c0709c9369cfc24391cdb2ee0b
vichka fonarev <fonarev.vichka@gmail.com>
2022-02-21 20:41:35 -0500

    temp commit, begin work on dependency lambda

---
+++ api/queryScore/dependency/queryDependency.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"api/util"
+	"context"
+	"log"
+	"strconv"
+
+	"github.com/aws/aws-lambda-go/events"
+	runtime "github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
+	for _, message := range sqsEvent.Records {
+		catalog := *message.MessageAttributes["catalog"].StringValue
+		owner := *message.MessageAttributes["owner"].StringValue
+		name := *message.MessageAttributes["name"].StringValue
+		timeFrame, err := strconv.Atoi(*message.MessageAttributes["timeFrame"].StringValue)
+
+		if err != nil {
+			log.Fatalln("Error converting time frame to int")
+		}
+		
+		util.QueryProject(catalog, owner, name, timeFrame)
+	}
+
+	return nil
+}
+
+func main() {
+	runtime.Start(handler)
+}
