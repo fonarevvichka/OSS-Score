c57597d3c464d02509b15521ae4108a7508dba33
Vichka Fonarev <fonarev.vichka@gmail.com>
2022-03-15 14:35:09 -0400

    Initial working version of website (#27)
    Working things:
    - Core react website is running locally with no errors
    - Website calls prod API to request scores
    - Website displays scores in raw format on the homepage
    - Website validates urls inserted
    - Other pages are not populated (pages not including home page)
    
    Co-authored-by: Eli Dow <eliasjohnsondow@gmail.com>
    Co-authored-by: epolak01 <56269269+epolak01@users.noreply.github.com>

---
+++ website/oss-score-site/src/components/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Categories.css';
+
+export default function Categories() {
+  return (
+    <div>
+      <header>Categories</header>
+      <div>Highest Rated</div>
+    </div>
+  )
+}
---
+++ website/oss-score-site/src/components/GenerateScores.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './GenerateScores.css';
+
+export default function GenerateScores() {
+  return (
+    <div>
+      <header>How We Generate Our Scores</header>
+      random.randint(0,1)
+    </div>
+  )
+}
