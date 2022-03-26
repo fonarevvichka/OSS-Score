299c2c3f63fc1145166c644ca5cdf568d87cd143
dalder1 <51690677+dalder1@users.noreply.github.com>
2022-03-26 16:59:34 -0400

    add privacy policy tab

---
+++ website/oss-score-site/src/components/PrivacyPolicy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './PrivacyPolicy.css';
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <header>We do not collect personal data</header>
+    </div>
+  )
+}
