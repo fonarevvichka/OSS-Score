5340e9440ccbcf4f973373edff12c9cb9108a17a
dalder1 <51690677+dalder1@users.noreply.github.com>
2022-03-26 17:12:02 -0400

    refine privacy policy

--- website/oss-score-site/src/components/PrivacyPolicy.js
+++ website/oss-score-site/src/components/PrivacyPolicy.js
@@ -5,7 +5,8 @@ import './PrivacyPolicy.css';
 export default function PrivacyPolicy() {
   return (
     <div>
-      <header>We do not collect personal data</header>
+      <header>We Do Not Collect Any Personal Data</header>
+      <p class="privacy">We don't store your data, period. We don't record your ip address, serve any cookies, or record your history of scores requested. Because we do not collect any personal information it can’t be stolen, demanded, leaked or abused.</p>
     </div>
   )
 }
