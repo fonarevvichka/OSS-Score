61e08ce5688ae2485594e467f6f0f56955bcfae2
dalder1 <51690677+dalder1@users.noreply.github.com>
2022-04-01 14:16:03 -0400

    add colored dot to extension (#104)
    * add color coded dot based on score to front of scores
    
    * clean up of display, make confidence for license and activity in same column

--- extension/extension.js
+++ extension/extension.js
@@ -78,6 +78,56 @@ async function awaitResults(scoreDiv, owner, repo) {
     });
 }
 
+function hslToRgb(h, s, l){
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255)];
+}
+
+function numberToColorHsl(i, min, max) {
+    var ratio = i;
+    if (min> 0 || max < 1) {
+        if (i < min) {
+            ratio = 0;
+        } else if (i > max) {
+            ratio = 1;
+        } else {
+            var range = max - min;
+            ratio = (i-min) / range;
+        }
+    }
+    
+    // as the function expects a value between 0 and 1, and red = 0° and green = 120°
+    // we convert the input to the appropriate hue value
+    var hue = ratio * 1.2 / 3.60;
+    //if (minMaxFactor!=1) hue /= minMaxFactor;
+    //console.log(hue);
+    
+    // we convert hsl to rgb (saturation 100%, lightness 50%)
+    var rgb = hslToRgb(hue, 1, .5);
+    // we format to css value and return
+    return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'; 
+}
+
 // insertHTML expects the div to insert the html into, the scores to be populated (may be null), and the 
 // state of the extension. Depending on the state it inserts the correct html and then returns nothing. 
 function insertHTML(scoreDiv, scores, state) {
@@ -94,9 +144,13 @@ function insertHTML(scoreDiv, scores, state) {
     }
     scoreDiv.innerHTML = header;
     if (state == "ready") {
+        let activityColor = numberToColorHsl(scores.activity.score/10, 0, 1);
+        scoreDiv.innerHTML += '<dot-extension style="color:' + activityColor + '">●</dot-extension>';
         scoreDiv.innerHTML += 'Activity: ' + (scores.activity.score).toFixed(1) + ' of 10';
         scoreDiv.innerHTML += '&nbsp; | &nbsp; Confidence: ' + scores.activity.confidence.toFixed(0) + '%';
         scoreDiv.innerHTML += '<br/><br/>';
+        let licenseColor = numberToColorHsl(scores.license.score/10, 0, 1);
+        scoreDiv.innerHTML += '<dot-extension style="color:' + licenseColor + '">●</dot-extension>';
         scoreDiv.innerHTML += 'License: ' + (scores.license.score).toFixed(1) + ' of 10';
         scoreDiv.innerHTML += '&nbsp; | &nbsp; Confidence: ' + scores.license.confidence.toFixed(0) + '%';
     } else if (state == "loading") {
