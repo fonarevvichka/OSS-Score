a77130f0e8fc8df57952c3f9a3ae21ebde9b7749
Eli Dow <eliasjohnsondow@gmail.com>
2022-04-03 16:50:08 -0400

    better navbar and new background image

--- website/oss-score-site/src/components/Navigation.js
+++ website/oss-score-site/src/components/Navigation.js
@@ -8,18 +8,16 @@ const Navigation = () => {
     return (
         <>
             <Navbar className='navigation-bar' expand="lg" variant="dark">
-                <Container>
-                <Navbar.Brand href="/">OSS-Score</Navbar.Brand>
-                    <Nav className="me-auto ml-0">
-                        <Nav.Link class="nav-home" href="/">Home</Nav.Link>
-                        <Nav.Link class="nav-link" href="/about">About</Nav.Link>
-                        <Nav.Link class="nav-link" href="/extension">Extension</Nav.Link>
-                        <Nav.Link class="nav-link" href="/generate-scores">How We Generate Score</Nav.Link>
-                        <Nav.Link class="nav-link" href="/categories">Categories</Nav.Link>
-                        <Nav.Link class="nav-link" href="/workforus">Work For Us</Nav.Link>
-                        <Nav.Link class="nav-link" href="/privacypolicy">Privacy Policy</Nav.Link>
-                    </Nav>
-                </Container>
+                <Navbar.Brand href="/" style={{marginLeft: "15px"}}>OSS-Score</Navbar.Brand>
+                <Nav className="me-auto ml-0">
+                    <Nav.Link class="nav-home" href="/">Home</Nav.Link>
+                    <Nav.Link class="nav-link" href="/about">About</Nav.Link>
+                    <Nav.Link class="nav-link" href="/extension">Extension</Nav.Link>
+                    <Nav.Link class="nav-link" href="/generate-scores">How We Score</Nav.Link>
+                    <Nav.Link class="nav-link" href="/categories">Categories</Nav.Link>
+                    <Nav.Link class="nav-link" href="/workforus">Work For Us</Nav.Link>
+                    <Nav.Link class="nav-link" href="/privacypolicy">Privacy Policy</Nav.Link>
+                </Nav>
             </Navbar>
         </>
     );
