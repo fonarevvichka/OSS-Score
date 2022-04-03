237bd4e7024c16224aaafc2c8d5cb270b77bfc80
Vichka Fonarev <fonarev.vichka@gmail.com>
2022-04-03 11:48:36 -0400

    removed workforus and categories tabs from routes, pages, and navbar, center avbar

--- website/oss-score-site/src/components/Navigation.js
+++ website/oss-score-site/src/components/Navigation.js
@@ -8,13 +8,11 @@ const Navigation = () => {
         <>
             <Navbar expand="lg" bg="primary" variant="dark">
                 <Container>
-                    <Nav className="ml-auto">
+                    <Nav className="m-auto">
                         <Nav.Link class="nav-home" href="/">Home</Nav.Link>
                         <Nav.Link class="nav-link" href="/about">About</Nav.Link>
                         <Nav.Link class="nav-link" href="/extension">Extension</Nav.Link>
                         <Nav.Link class="nav-link" href="/generate-scores">How We Generate Score</Nav.Link>
-                        <Nav.Link class="nav-link" href="/categories">Categories</Nav.Link>
-                        <Nav.Link class="nav-link" href="/workforus">Work For Us</Nav.Link>
                         <Nav.Link class="nav-link" href="/privacypolicy">Privacy Policy</Nav.Link>
                     </Nav>
                 </Container>
