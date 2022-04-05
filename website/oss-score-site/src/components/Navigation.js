2038d924b28bcdeeeadaea49219b6fa107efc184
epolak01 <56269269+epolak01@users.noreply.github.com>
2022-04-05 16:51:08 -0400

    Trying new nav-bar but its messed up a bit

--- website/oss-score-site/src/components/Navigation.js
+++ website/oss-score-site/src/components/Navigation.js
@@ -7,24 +7,59 @@ import smallLogo from '../images/favicon.ico'
 
 const Navigation = () => {
     return (
-        <>
-            <Navbar className='navigation-bar' expand="lg" variant="dark">
-                <Container>
-                <Navbar.Brand href="/">
-                    <img className='navLogo' src={smallLogo}></img>
-                    OSS-Score</Navbar.Brand>
-                    <Nav className="me-auto ml-0">
-                    <Nav className="m-auto ml-0">
-                        <Nav.Link class="nav-home" href="/">Home</Nav.Link>
-                        <Nav.Link class="nav-link" href="/about">About</Nav.Link>
-                        <Nav.Link class="nav-link" href="/extension">Extension</Nav.Link>
-                        <Nav.Link class="nav-link" href="/generate-scores">How We Generate Score</Nav.Link>
-                        <Nav.Link class="nav-link" href="/privacypolicy">Privacy Policy</Nav.Link>
-                    </Nav>   
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
+        // <div className='container'>
+        <nav className="navbar navbar-expand-sm navbar-dark" style={{backgroundColor:'#5b4da2'}}>
+                <a href='/# ' className="navbar-brand mb-0 h1">
+                        <img className='d-inline-block align-top' src={smallLogo} width="30" height="30" alt='OSS-Logo'></img>
+                        OSS-Score
+                </a>
+                <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" className="navbar-toggler" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul classsName="navbar-nav">
+                        <li class="nav-item active">
+                            <a href="/# " className="nav-link">Home</a>
+                        </li>
+                        <li class="nav-item active">
+                                <a href="/about " className="nav-link">About</a>
+                        </li>
+                        <li class="nav-item active">
+                                <a href="/extension " className="nav-link">Extension</a>
+                        </li>
+                        <li class="nav-item active">
+                                <a href="/generate-scores " className="nav-link">Score Generation</a>
+                        </li>
+                        <li class="nav-item active">
+                                <a href="/privacy-policy " className="nav-link">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        // </div>
+
+
+        // <>
+        //     <Navbar className='navigation-bar' expand="lg" variant="dark">
+        //         <Container>
+        //         <Navbar.Brand href="/">
+        //             <img className='navLogo' src={smallLogo}></img>
+        //             OSS-Score</Navbar.Brand>
+        //             {/* <Nav className="me-auto ml-0"> */}
+        //             <Nav className="navbar-nav ms-auto">
+        //                 {/* <Navbar.Brand href="/">
+        //                     <img className='navLogo' src={smallLogo}></img>
+        //                     OSS-Score</Navbar.Brand> */}
+        //                 {/* <Nav.Link class="nav-home" href="/">Home</Nav.Link> */}
+        //                 <Nav.Link class="nav-link" href="/about">About</Nav.Link>
+        //                 <Nav.Link class="nav-link" href="/extension">Extension</Nav.Link>
+        //                 <Nav.Link class="nav-link" href="/generate-scores">How We Generate Score</Nav.Link>
+        //                 <Nav.Link class="nav-link" href="/privacypolicy">Privacy Policy</Nav.Link>
+        //             {/* </Nav>    */}
+        //             </Nav>
+        //         </Container>
+        //     </Navbar>
+        // </>
     );
 }
 
