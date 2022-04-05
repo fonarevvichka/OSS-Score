ab73b2fa3f047c8e46393eb302a067de1e9414ee
epolak01 <56269269+epolak01@users.noreply.github.com>
2022-04-05 13:08:04 -0400

    Regenerated package.lock file

--- website/oss-score-site/src/components/Navigation.js
+++ website/oss-score-site/src/components/Navigation.js
@@ -14,7 +14,7 @@ const Navigation = () => {
                     <img className='navLogo' src={smallLogo}></img>
                     OSS-Score</Navbar.Brand>
                     <Nav className="me-auto ml-0">
-                    <Nav className="m-auto">
+                    <Nav className="m-auto ml-0">
                         <Nav.Link class="nav-home" href="/">Home</Nav.Link>
                         <Nav.Link class="nav-link" href="/about">About</Nav.Link>
                         <Nav.Link class="nav-link" href="/extension">Extension</Nav.Link>
