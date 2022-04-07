09c1950d0a49be0b3f82a92076189f3bbab80438
epolak01 <56269269+epolak01@users.noreply.github.com>
2022-04-07 13:06:24 -0400

    applied simple styling to privacy policy page (#171)

--- website/oss-score-site/src/components/PrivacyPolicy.js
+++ website/oss-score-site/src/components/PrivacyPolicy.js
@@ -4,9 +4,14 @@ import './PrivacyPolicy.css';
 
 export default function PrivacyPolicy() {
   return (
-    <div>
+    <div className='wrapper'>
+    <div className="privacy-body">
       <header>We Do Not Collect Any Personal Data</header>
-      <p class="privacy">We don't store your data, period. We don't record your ip address, serve any cookies, or record your history of scores requested. Because we do not collect any personal information it can’t be stolen, demanded, leaked or abused.</p>
+        <p className="privacy">&emsp; We don't store your data, period. 
+        We don't record your ip address, serve any cookies, or record your history 
+        of scores requested. Because we do not collect any personal information it can’t 
+        be stolen, demanded, leaked or abused.</p>
+    </div>
     </div>
   )
 }
