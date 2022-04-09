bec1ad249f3d2d814a864b1ca735c5bc9c8419ff
epolak01 <56269269+epolak01@users.noreply.github.com>
2022-04-09 14:35:18 -0400

    Fixed positioning and turned text white

--- website/oss-score-site/src/components/NotFound.js
+++ website/oss-score-site/src/components/NotFound.js
@@ -6,11 +6,10 @@ import './NotFound.css';
 export default function NotFound() {
     return (
          <div className='wrapper'>
-    <div className="privacy-body">
-        <div className='bubbles'> <GiBubbles size={40}/> </div>
-        <br /><p className="privacy">Go fish... this page does not exist</p>
-        <div className='fish'> <FaFish size={40}/> </div>
-    </div>
+    <div className="not-found-body">
+        <br /><p className="not-found-text">Go fish... this page does not exist <div className='bubbles'> <GiBubbles size={40} /> </div></p>
+        <div className='fish'> <FaFish size={40} /> </div>
+        </div>
     </div>
     )
 }
