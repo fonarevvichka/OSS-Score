c425f6ec99522964b1afccdabcb9fdb32be63053
epolak01 <56269269+epolak01@users.noreply.github.com>
2022-04-09 14:09:45 -0400

    Styled page not found page

--- website/oss-score-site/src/components/NotFound.js
+++ website/oss-score-site/src/components/NotFound.js
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import { FaFish } from "react-icons/fa";
+import { GiBubbles } from "react-icons/gi";
 import './NotFound.css';
 
 export default function NotFound() {
     return (
-        <div>
-            Page Does Not Exist You Clown
-        </div>
+         <div className='wrapper'>
+    <div className="privacy-body">
+        <div className='bubbles'> <GiBubbles size={40}/> </div>
+        <br /><p className="privacy">Go fish... this page does not exist</p>
+        <div className='fish'> <FaFish size={40}/> </div>
+    </div>
+    </div>
     )
 }
