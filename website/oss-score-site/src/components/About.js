8d742ccd39396eebd68f4e621ae4e185ed7fa36c
epolak01 <56269269+epolak01@users.noreply.github.com>
2022-04-10 11:51:13 -0400

    updated text and added internal links to about page

--- website/oss-score-site/src/components/About.js
+++ website/oss-score-site/src/components/About.js
@@ -13,42 +13,42 @@ const About = () => {
       <header>Our Solution</header>
       <p className='solution'>
         <p>&emsp; We provide developers with a comprehensive activity and license score, that is calculated from a wide variety of GitHub metadata for that repository and its dependencies. This ensures developers can quickly and easily pick repositories that are well maintained and won’t cause any headaches in the future.</p>
-        <p>&emsp; For quick “at-a-glance” information the OSS-Score chrome extension embeds scores directly into the GitHub repository homepage.</p>
+        <p>&emsp; For quick “at-a-glance” information the <a className='internal-link' href='/extension'>OSS-Score chrome extension </a> embeds scores directly into the GitHub repository homepage.</p>
         <p className='image-container'><img alt='extension-img'></img></p>
-        <p>&emsp; For a deeper dive the OSS-Score website allows users to compare the scores and individual metrics of different open source projects and tools.</p>
+        <p>&emsp; For a deeper dive the <a className='internal-link' href='/'>OSS-Score website </a> allows users to compare the scores and individual metrics of different open source projects and tools.</p>
         <p className='image-container'><img alt='website-img'></img></p>
       </p>
       <header>Meet the Team</header>
       <div className='team-image-container'><img alt='OSS-Score Team Image'></img></div>
       <p className='meet-the-team'>
-        <p className='team-blurb'>&emsp; The OSS-Score team is composed of 4 senior computer science majors from Tufts University. The first (and current) iteration of OSS-Score was built as a senior capstone project sponsored by Ole Lensmar (CTO of Kubeshop).</p>  
+        <p className='team-blurb'>&emsp; OSS-Score was created by Vichka Fonarev, Eli Dow, Daniel Alderman, and Emil Polakiewicz as a senior computer science capstone project at Tufts University under the mentorship of Ole Lensmar.</p>  
           <div className='team-members'>
           <p className='team-member'>
             <div className='name'>Vichka Fonarev</div>
             <div className='photo'><img alt='vichka-pic'></img></div>
             <div className='blurb'>Okay I guess</div>
-            <div className='linkedin'><a href='#'><FaLinkedin /></a></div>
+            <div className='linkedin'><a target="_blank" href='#'><FaLinkedin /></a></div>
           </p>
 
           <p className='team-member'>
             <div className='name'>Eli Dow</div>
             <div className='photo'><img alt='eli-pic'></img></div>
             <div className='blurb'>He's fine</div>
-            <div className='linkedin'><a href='#'><FaLinkedin /></a></div>
+            <div className='linkedin'><a target="_blank" href='#'><FaLinkedin /></a></div>
           </p>
 
           <p className='team-member'>
             <div className='name'>Daniel Alderman</div>
             <div className='photo'><img alt='doniel-pic'></img></div>
             <div className='blurb'>Ew</div>
-            <div className='linkedin'><a href='#'><FaLinkedin /></a></div>
+            <div className='linkedin'><a target="_blank" href='#'><FaLinkedin /></a></div>
           </p>
 
           <p className='team-member'>
             <div className='name'>Emil Polakiewicz</div>
             <div className='photo'><img alt='emil-pic'></img></div>
             <div className='blurb'>Pretty cool guy actually</div>
-            <div className='linkedin'><a href='#'><FaLinkedin /></a></div>
+            <div className='linkedin'><a target="_blank" href='https://www.linkedin.com/in/emil-polakiewicz-12887b17b/'><FaLinkedin /></a></div>
           </p>
         </div>
       </p>
