3302f874e24199e5fd3b465670d1e2b377d722ce
Eli Dow <eliasjohnsondow@gmail.com>
2022-04-11 16:04:43 -0400

    consistend tab color and updated collapse to stay on screen

--- website/oss-score-site/src/components/Navigation.js
+++ website/oss-score-site/src/components/Navigation.js
@@ -7,22 +7,37 @@ import smallLogo from '../images/favicon.ico'
 import gitLogo from '../images/Git-Icon-White.png'
 
 const Navigation = () => {
+
+    const changeTabColor = (hover) => (event) => {
+        if (hover === "hover") {
+            event.target.style.color = "white"
+        } else {
+            event.target.style.color = "#FFFFFF8C"
+        }
+    }
+
     return (
-            <Navbar className='navigation-bar' expand="md" variant="dark">
+            <Navbar className='navigation-bar' expand="lg" variant="dark">
                 <Navbar.Brand href="/" style={{paddingLeft:"10px"}}>
                     <img className='navLogo' src={smallLogo}></img>
-                    OSS-Score
+                    <div className='navLogoText'>OSS-Score</div>
                 </Navbar.Brand>
 
                     <Nav className="navbar-nav ms-auto">
-                        <Nav.Link class="nav-home" href="/">Home</Nav.Link>
-                        <Nav.Link class="nav-link" href="/about">About</Nav.Link>
-                        <Nav.Link class="nav-link" href="/extension">Extension</Nav.Link>
-                        <Nav.Link class="nav-link" href="/generate-scores">How We Generate Scores</Nav.Link>
-                        <Nav.Link class="nav-link" href="/privacypolicy">Privacy Policy</Nav.Link>
+                        <Nav.Link class="nav-home" href="/" style={{color:"#FFFFFF8C"}}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>Home</Nav.Link>
+                        <Nav.Link class="nav-link" href="/about" style={{color:"#FFFFFF8C"}}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>About</Nav.Link>
+                        <Nav.Link class="nav-link" href="/extension" style={{color:"#FFFFFF8C"}}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>Extension</Nav.Link>
+                        <Nav.Link class="nav-link" href="/generate-scores" style={{color:"#FFFFFF8C"}}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>How We Score</Nav.Link>
+                        <Nav.Link class="nav-link" href="/privacypolicy" style={{color:"#FFFFFF8C"}}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>Privacy Policy</Nav.Link>
                         <Navbar.Brand href="https://github.com/fonarevvichka/OSS-Score"
                             target="_blank" class="OSSScoreLink"
-                            style={{ paddingLeft: "10px", fontSize: "15px" }}>
+                            style={{ paddingLeft: "10px", fontSize: "15px", color:"#FFFFFF8C" }}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>
                             <img className='gitLogo' src={gitLogo}></img>
                             fonarevvichka/OSS-Score
                         </Navbar.Brand>
