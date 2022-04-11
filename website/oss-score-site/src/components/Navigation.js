e5f6a6940742a1a15e33b1b60dd710b6b4473349
elidow <56176047+elidow@users.noreply.github.com>
2022-04-11 17:20:48 -0400

    added link to oss-score repo in navbar (#189)
    * added link to oss-score repo in navbar
    
    * Moved repo link to right side of nav bar
    
    * consistend tab color and updated collapse to stay on screen
    
    * consistent tab color and updated collapse to stay on screen
    
    * Adjusted color and size of git logo and made it into a react icon
    
    * made oss-score logo consistent with other nav bar items
    
    Co-authored-by: epolak01 <56269269+epolak01@users.noreply.github.com>
    Co-authored-by: Vichka Fonarev <fonarev.vichka@gmail.com>

--- website/oss-score-site/src/components/Navigation.js
+++ website/oss-score-site/src/components/Navigation.js
@@ -1,22 +1,47 @@
 import React from 'react'
-import { Navbar, Container, Nav} from 'react-bootstrap'
+import { Navbar, Nav} from 'react-bootstrap'
 import './Navigation.css';
 
 import './Navigation.css';
 import smallLogo from '../images/favicon.ico'
+import { ImGit } from "react-icons/im";
 
 const Navigation = () => {
+
+    const changeTabColor = (hover) => (event) => {
+        if (hover === "hover") {
+            event.target.style.color = "white"
+        } else {
+            event.target.style.color = "#FFFFFF8C"
+        }
+    }
+
     return (
-            <Navbar className='navigation-bar' expand="md" variant="dark">
-                <Navbar.Brand href="/" style={{paddingLeft:"10px"}}>
-                    <img className='navLogo' src={smallLogo}></img>
-                    OSS-Score</Navbar.Brand>
+            <Navbar className='navigation-bar' expand="lg" variant="dark">
+            <Nav.Link className='oss-logo' href="/" style={{ fontSize: "20px", color: "#FFFFFF8C" }}
+                onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>
+                <img className='navLogo' src={smallLogo}></img> OSS-Score
+            </Nav.Link>
+
+
                     <Nav className="navbar-nav ms-auto">
-                        <Nav.Link class="nav-home" href="/">Home</Nav.Link>
-                        <Nav.Link class="nav-link" href="/about">About</Nav.Link>
-                        <Nav.Link class="nav-link" href="/extension">Extension</Nav.Link>
-                        <Nav.Link class="nav-link" href="/generate-scores">How We Generate Scores</Nav.Link>
-                        <Nav.Link class="nav-link" href="/privacypolicy">Privacy Policy</Nav.Link>
+                        <Nav.Link class="nav-home" href="/" style={{color:"#FFFFFF8C"}}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>Home</Nav.Link>
+                        <Nav.Link class="nav-link" href="/about" style={{color:"#FFFFFF8C"}}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>About</Nav.Link>
+                        <Nav.Link class="nav-link" href="/extension" style={{color:"#FFFFFF8C"}}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>Extension</Nav.Link>
+                        <Nav.Link class="nav-link" href="/generate-scores" style={{color:"#FFFFFF8C"}}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>How We Score</Nav.Link>
+                        <Nav.Link class="nav-link" href="/privacypolicy" style={{color:"#FFFFFF8C"}}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>Privacy Policy</Nav.Link>
+                        
+                        <Nav.Link href="https://github.com/fonarevvichka/OSS-Score"
+                            target="_blank" class="OSSScoreLink"
+                            style={{color: "#FFFFFF8C" }}
+                            onMouseEnter={changeTabColor("hover")} onMouseLeave={changeTabColor("")}>
+                            <ImGit size={21}/> fonarevvichka/OSS-Score
+                        </Nav.Link>
                     </Nav>
             </Navbar>
     );
