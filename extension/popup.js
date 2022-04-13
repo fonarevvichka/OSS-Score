d9bd791d46c90d1a708febce2c898186cc576c24
dalder1 <51690677+dalder1@users.noreply.github.com>
2022-04-12 20:42:17 -0400

    styling and bug fix

--- extension/popup.js
+++ extension/popup.js
@@ -1,38 +1,47 @@
 function changeTimeFrame() {
     var inputResult = document.getElementById("newTimeFrame").value;
-    if (inputResult != "") {
-        chrome.storage.sync.set({key: inputResult}, function() {
-            console.log('time frame is set to ' + inputResult);
-        });    
-        document.getElementById("newTimeFrame").value = '';
-        updateTimeFrame(inputResult);
-        sendMessage(inputResult);
+    if(isInt(inputResult)){
+        if (inputResult != "") {
+            chrome.storage.sync.set({key: inputResult}, function() {
+                console.log('time frame is set to ' + inputResult);
+            });    
+            document.getElementById("newTimeFrame").value = '';
+            sendMessage(inputResult);
+        }      
+    } else {
+        alert("Error: time frame must be a positive integer");
     }
+
+}
+
+function isInt(str) {
+    return /^[1-9]\d*$/.test(str);
 }
 
 function sendMessage(timeFrame) {
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
         var activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, {"timeFrame": timeFrame}, function(response) {
-            // TODO handle/silence error in html when user on wrong tab
-            try {
+            if (!window.chrome.runtime.lastError) {
                 console.log(response.message);
-            } catch (error) {
-                console.log(error);
+                updateTimeFrame(timeFrame);
+            } else {
+                console.log(chrome.runtime.lastError.message);
                 alert("Error: time frame could not be submitted. Make sure you are on the github tab.");
             }
+
         });
     });
 }
 
 function updateTimeFrame(timeFrame) {
     if (timeFrame == null) {
-        document.getElementById('current').innerHTML = "Current Time Frame is infinite";
+        document.getElementById('current').innerHTML = "Current Time Frame: Infinite";
     }
     else if (timeFrame == "1") {
-        document.getElementById('current').innerHTML = "Current Time Frame is " + timeFrame + " month";
+        document.getElementById('current').innerHTML = "Current Time Frame: " + timeFrame + " month";
     } else {
-        document.getElementById('current').innerHTML = "Current Time Frame is " + timeFrame + " months";
+        document.getElementById('current').innerHTML = "Current Time Frame: " + timeFrame + " months";
     }
 }
 
@@ -46,9 +55,10 @@ function resetTimeFrame() {
 
 document.addEventListener('DOMContentLoaded', function() {
     var buttons = document.querySelectorAll('button');
-    console.log(buttons.length);
-    buttons[0].addEventListener('click', changeTimeFrame, false); // submit button
-    buttons[1].addEventListener('click', resetTimeFrame, false); // reset button
+    var submit = document.getElementById('submit');
+    var reset = document.getElementById('reset');
+    submit.addEventListener('click', changeTimeFrame, false); // submit button
+    reset.addEventListener('click', resetTimeFrame, false); // reset button
     chrome.storage.sync.get(['key'], function(result) {
         if (result.key != null) { // there is a value stored
             updateTimeFrame(result.key);
