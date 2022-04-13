6776a741413cde619111ef3d2691a176a3a10b43
epolak01 <56269269+epolak01@users.noreply.github.com>
2022-04-12 21:35:32 -0400

    Added magnify effect to extension picture on about page and cleaned up image files we werent using

--- website/oss-score-site/src/components/About.js
+++ website/oss-score-site/src/components/About.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { FaLinkedin } from "react-icons/fa";
-import { GiMagnifyingGlass } from "react-icons/gi";
 import './About.css';
 
 
@@ -15,20 +14,14 @@ const About = () => {
       <div className='solution'>
         <div>&emsp; We provide developers with a comprehensive activity and license score, that is calculated from a wide variety of GitHub metadata for that repository and its dependencies. This ensures developers can quickly and easily pick repositories that are well maintained and won’t cause any headaches in the future.</div>
         <div>&emsp; For quick “at-a-glance” information the <a className='internal-link' href='/extension'>OSS-Score chrome extension </a> embeds scores directly into the GitHub repository homepage.</div>
-        {/* <div class="parent">
+        <div class="parent">
           <img class="image1 " alt='extension-img' src={require('../images/extension_pic.png')} />
           <img class="image2" alt='extension-closeup-img' src={require('../images/extension_closeup_pic.png')} />
-          <GiMagnifyingGlass className='magnify' size={40}/>
-        </div> */}
-        
-        
-        <div className='image-container'><img alt='extension-img' src={require('../images/extension_pic.png')}></img></div>
-        <div className='image-container-closeup'><img alt='extension-closeup-img' src={require('../images/extension_closeup_pic.png')}></img></div>
+        </div>
         <div>&emsp; For a deeper dive the <a className='internal-link' href='/'>OSS-Score website </a> allows users to compare the scores and individual metrics of different open source projects and tools.</div>
         <div className='image-container'><img alt='website-img' src={require('../images/website_pic.png')}></img></div>
       </div>
       <header>Meet the Team</header>
-      {/* <div className='team-image-container'><img alt='OSS-Score Team Image'></img></div> */}
       <div className='meet-the-team'>
         <div className='team-blurb'>&emsp; OSS-Score was created by Vichka Fonarev, Eli Dow, Daniel Alderman, and Emil Polakiewicz as a senior computer science capstone project at Tufts University. Open source enthusiast and current CTO of Kubeshop Ole Lensmar provided invaluable mentorship throughout the creation of OSS-Score.</div>  
         <br />
@@ -36,28 +29,24 @@ const About = () => {
             <div className='team-member'>
               <div className='name'>Vichka Fonarev</div>
             <div className='photo'><img alt='vichka-pic' src={require('../images/vichka.png')}></img></div>
-              {/* <div className='blurb'>Okay I guess</div> */}
             <div className='linkedin'><a target="_blank" href='https://www.linkedin.com/in/vichka-fonarev-b1b980110/'><FaLinkedin /></a></div>
             </div>
 
             <div className='team-member'>
               <div className='name'>Eli Dow</div>
             <div className='photo'><img alt='eliIsABum' src={require('../images/eli.png')}></img></div>
-              {/* <div className='blurb'>He's fine</div> */}
             <div className='linkedin'><a target="_blank" href='https://www.linkedin.com/in/eli-dow-93105b168/'><FaLinkedin /></a></div>
             </div>
 
             <div className='team-member'>
               <div className='name'>Daniel Alderman</div>
             <div className='photo'><img alt='doniel-pic' src={require('../images/daniel.png')}></img></div>
-              {/* <div className='blurb'>Ew</div> */}
             <div className='linkedin'><a target="_blank" href='https://www.linkedin.com/in/daniel-alderman-ab88321a1/'><FaLinkedin /></a></div>
             </div>
 
             <div className='team-member'>
               <div className='name'>Emil Polakiewicz</div>
             <div className='photo'><img alt='emil-pic' src={require('../images/emil.png')}></img></div>
-              {/* <div className='blurb'>Pretty cool guy actually</div> */}
               <div className='linkedin'><a target="_blank" href='https://www.linkedin.com/in/emil-polakiewicz-12887b17b/'><FaLinkedin /></a></div>
             </div>
         </div>
